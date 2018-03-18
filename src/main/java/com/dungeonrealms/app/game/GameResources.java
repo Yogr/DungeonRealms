78042cf13fafc4ba9b12bf525b425801770bcc98
Dan Hartnett <danhartnett@Alexiss-MacBook-Pro.local>
2018-03-17 18:51:44 -0700

    Cleanup on GameResources that I left out

--- src/main/java/com/dungeonrealms/app/game/GameResources.java
+++ src/main/java/com/dungeonrealms/app/game/GameResources.java
@@ -11,14 +11,8 @@ import lombok.Getter;
 import lombok.SneakyThrows;
 
 import java.io.InputStream;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Enumeration;
 import java.util.Map;
 
-import static java.lang.ClassLoader.getSystemResources;
 
 @Getter
 public class GameResources {
@@ -39,39 +33,6 @@ public class GameResources {
     @SneakyThrows
     private <T> T loadResourceMap(String fileName, Class<?> clazz) {
         InputStream resource = getClass().getClassLoader().getResourceAsStream(fileName);
-        System.out.println("res1 is " + resource);
-
-        if (resource == null) {
-            resource = getClass().getClassLoader().getResourceAsStream("/" + fileName);
-            System.out.println("res2 is " + resource);
-        }
-
-        if (resource == null) {
-            resource = getClass().getClassLoader().getResourceAsStream("./" + fileName);
-            System.out.println("res3 is " + resource);
-        }
-
-        //if (resource == null) {
-            Enumeration<URL> enumeration = getSystemResources("./");
-            while (enumeration.hasMoreElements()) {
-                URL url = enumeration.nextElement();
-                System.out.println("Resource: " + url.getFile());
-            }
-
-            Enumeration<URL> enumeration2 = getClass().getClassLoader().getResources("./");
-            while (enumeration2.hasMoreElements()) {
-                URL url = enumeration2.nextElement();
-                System.out.println("2Resource: " + url.getFile());
-            }
-        //}
-
-        if (resource == null) {
-            Path path = Paths.get(getClass().getResource("/").toURI());//.resolve(fileName);
-            System.out.println("Path is " + path);
-            resource = Files.newInputStream(path.resolve("resources/" + fileName));
-            System.out.println("Resource is " + resource);
-        }
-
         ObjectMapper mapper = Config.getInstance().getJacksonMapper();
         JavaType type = mapper.getTypeFactory().constructMapType(Map.class, String.class, clazz);
 
